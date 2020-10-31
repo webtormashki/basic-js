7f1bd0f957fdc948740cd3d1862e165b5aaf06f7
Tanya <webtormashki@gmail.com>
2020-10-31 16:49:45 +0300

    feat: implement createDreamTeam algorithm

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  var cleared = Array();
+  // members = members.sort();
+  for (var i = 0; i < members.length; i++) {
+    if (typeof members[i] != 'string') {
+      continue;
+    }
+    cleared.push(members[i].trim().charAt(0).toUpperCase());
+  }
+  return cleared.sort().join('');
 };
